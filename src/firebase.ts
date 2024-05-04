9c24c8a1c712cdabf36b01a43b0d37c6d16c0246
Daniel Godin <DanielEGodin@gmail.com>
2024-05-04 15:44:28 -0700

    firebase changes

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,9 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { firebaseConfig } from '../firebase-config';
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+// Firebase Emulators:
+connectFirestoreEmulator(db, '127.0.0.1', 8080);
+connectAuthEmulator(auth, "http://127.0.0.1:9099");
