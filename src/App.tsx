046c3da2d67c78656c335487e2c74151ba43bf4e
Daniel Godin <DanielEGodin@gmail.com>
2024-05-05 07:25:57 -0700

    Created basic React set up for Nav, Main, and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,37 @@ function App() {
 
   return (
     <>
-
+      <NavUI />
+      <MainUI />
+      <FooterUI />
     </>
   )
 }
 
 export default App
+
+function MainUI() {
+  return (
+    <main>
+      <h1>Welcome to Household Organizer</h1>
+    </main>
+  )
+}
+
+
+function NavUI() {
+  return (
+    <nav>
+      <header>Household Organizer</header>
+      <a href=''>Log In / Sign Up (Profile Button If Logged In)</a>
+    </nav>
+  )
+}
+
+function FooterUI() {
+  return (
+    <footer>
+      <p>Created by <a href='https://danielgodin.org'>Daniel Godin</a> & Joanna Lew</p>
+    </footer>
+  )
+}
